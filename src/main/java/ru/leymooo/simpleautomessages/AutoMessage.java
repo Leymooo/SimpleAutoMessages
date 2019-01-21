71132b5eb3be61618d59a6bece153b24727baee5
Leymooo <mikimouse100@mail.ru>
2019-01-21 20:28:45 +0200

    SimpleAutoMessages 1.0

---
+++ src/main/java/ru/leymooo/simpleautomessages/AutoMessage.java
@@ -0,0 +1,166 @@
+package ru.leymooo.simpleautomessages;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.velocitypowered.api.scheduler.ScheduledTask;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.kyori.text.Component;
+import net.kyori.text.serializer.ComponentSerializers;
+import net.md_5.bungee.config.Configuration;
+
+public class AutoMessage {
+
+    private static final Pattern RANGE_MATCH = Pattern.compile("(.+)\\{(\\d+)-(\\d+)\\}");
+
+    private final Random random = new Random();
+
+    private final ProxyServer proxyServer;
+    private final List<RegisteredServer> servers = new ArrayList<>();
+    private final List<MessageContainer> messages = new ArrayList<>();
+    private final int interval;
+    private final boolean shuffle;
+    private boolean global;
+
+    private ScheduledTask task;
+    private int index = -1;
+
+    public AutoMessage(ProxyServer server, List<String> servers, int interval, boolean shuffle, List<String> messages) {
+        this.proxyServer = server;
+        this.interval = interval;
+        this.shuffle = shuffle;
+        messages.forEach(message -> this.messages.add(new MessageContainer(message)));
+        parseServers(servers);
+    }
+
+    private void parseServers(List<String> servers) {
+        servers.stream().filter(server -> "global".equalsIgnoreCase(server)).findAny().ifPresent(s -> this.global = true);
+        if (!global) {
+            servers.forEach(server -> {
+                Matcher matcher = RANGE_MATCH.matcher(server);
+                if (matcher.find()) {
+                    String name = matcher.group(1);
+                    int lower = Integer.parseInt(matcher.group(2));
+                    int upper = Integer.parseInt(matcher.group(3));
+                    for (int i = lower; i <= upper; i++) {
+                        proxyServer.getServer(name + i).ifPresent(this.servers::add);
+                    }
+                } else {
+                    proxyServer.getServer(server).ifPresent(this.servers::add);
+                }
+            });
+        }
+    }
+
+    public CheckResult checkAndRun(PluginContainer plugin) {
+        if (messages.isEmpty()) {
+            return CheckResult.NO_MESSAGES;
+        }
+        if (!global && servers.isEmpty()) {
+            return CheckResult.NO_SERVERS;
+        }
+        if (interval <= 0) {
+            return CheckResult.INTERVAL_NOT_SET;
+        }
+        if (task != null) {
+            return CheckResult.ALREADY_RUNNING;
+        }
+        this.task = proxyServer.getScheduler().buildTask(plugin, () -> {
+            checkIndex();
+            MessageContainer message = messages.get(index);
+            if (global) {
+                if (!message.hasPlaceholders()) {
+                    proxyServer.broadcast(message.getForPlayer(null));
+                } else {
+                    proxyServer.getAllPlayers().forEach(pl -> pl.sendMessage(message.getForPlayer(pl)));
+                }
+            } else {
+                this.servers.forEach(srv -> srv.getPlayersConnected().forEach(pl -> pl.sendMessage(message.getForPlayer(pl))));
+            }
+            index++;
+        }).repeat(interval, TimeUnit.SECONDS).schedule();
+        return CheckResult.OK;
+    }
+
+    public void stopScheduler() {
+        if (this.task != null) {
+            this.task.cancel();
+            this.task = null;
+        }
+    }
+
+    private void checkIndex() {
+        if (index == -1 || index >= messages.size()) {
+            index = 0;
+            if (shuffle) {
+                Collections.shuffle(messages, random);
+            }
+        }
+    }
+
+    public static AutoMessage fromConfiguration(Configuration configuration, ProxyServer server) {
+        return new AutoMessage(server,
+                configuration.getStringList("servers"),
+                configuration.getInt("interval", 0),
+                configuration.getBoolean("random", false),
+                configuration.getStringList("messages"));
+    }
+
+    public static enum CheckResult {
+        OK,
+        NO_SERVERS,
+        NO_MESSAGES,
+        INTERVAL_NOT_SET,
+        ALREADY_RUNNING;
+    }
+
+    private static class MessageContainer {
+
+        private final static JsonParser JSON_PARSER = new JsonParser();
+
+        private final String message;
+        private final Component preCreated;
+        private final boolean isJson;
+
+        public MessageContainer(String message) {
+            this.message = message;
+            JsonElement parsed = null;
+            try {
+                parsed = JSON_PARSER.parse(message);
+            } catch (JsonSyntaxException ex) {
+            }
+            isJson = parsed != null && !parsed.isJsonPrimitive();
+            preCreated = (message.contains("%player%") || message.contains("%server%")) ? null : create(message);
+        }
+
+        public boolean hasPlaceholders() {
+            return preCreated == null;
+        }
+
+        public Component getForPlayer(Player player) {
+            if (!hasPlaceholders()) {
+                return preCreated;
+            }
+            Optional<ServerConnection> server = player.getCurrentServer();
+            String s_server = server.isPresent() ? server.get().getServerInfo().getName() : "<none>";
+            return create(message.replace("%player%", player.getUsername()).replace("%server%", s_server));
+        }
+
+        private Component create(String text) {
+            return isJson ? ComponentSerializers.JSON.deserialize(text) : ComponentSerializers.LEGACY.deserialize(text, '&');
+        }
+
+    }
+}
---
+++ src/main/java/ru/leymooo/simpleautomessages/SimpleAutoMessages.java
@@ -0,0 +1,142 @@
+package ru.leymooo.simpleautomessages;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyReloadEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.scheduler.ScheduledTask;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+@Plugin(id = "simpleautomessages", name = "SimpleAutoMessages", version = "1.0",
+        description = "AutoMessages plugin for velocity",
+        authors = "Leymooo")
+public class SimpleAutoMessages {
+
+    private final ProxyServer server;
+    private final Logger logger;
+    private final Path dataDirectory;
+    private Configuration config;
+    private ScheduledTask task;
+    private final List<AutoMessage> messages;
+
+    @Inject
+    public SimpleAutoMessages(ProxyServer server, Logger logger, @DataDirectory Path userConfigDirectory) {
+        this.server = server;
+        this.logger = logger;
+        this.dataDirectory = userConfigDirectory;
+        this.messages = new ArrayList<>();
+    }
+
+    @Subscribe
+    public void onProxyInitialize(ProxyInitializeEvent event) {
+        logger.info("Loading config");
+        if (!loadConfig() || config == null) {
+            logger.error("Config is not loaded. Plugin will be inactive");
+            return;
+        }
+        logger.info("Config loaded");
+        logger.info("Plugin will be enabled after 3 seconds");
+        createAutoMessages();
+    }
+
+    @Subscribe
+    public void onProxyReload(ProxyReloadEvent event) {
+        logger.info("Reloading");
+        if (!loadConfig() || config == null) {
+            logger.error("Can not reload config");
+            return;
+        }
+        messages.forEach(AutoMessage::stopScheduler);
+        logger.info("Plugin will be reloaded after 3 seconds");
+        if (task != null) {
+            task.cancel();
+        }
+        createAutoMessages();
+    }
+
+    @Subscribe
+    public void onShutDown(ProxyShutdownEvent ev) {
+        logger.info("Disabling SimpleAutoMessages");
+        messages.forEach(AutoMessage::stopScheduler);
+        logger.info("SimpleAutoMessages disabled");
+    }
+
+    private void createAutoMessages() {
+        this.task = server.getScheduler().buildTask(this, () -> {
+            logger.info("Staring AutoMessages tasks");
+            synchronized (messages) {
+                messages.clear();
+                for (String section : config.getKeys()) {
+                    AutoMessage am = AutoMessage.fromConfiguration(config.getSection(section), server);
+                    AutoMessage.CheckResult result = am.checkAndRun(server.getPluginManager().fromInstance(SimpleAutoMessages.this).get());
+                    switch (result) {
+                        case OK:
+                            logger.info("'{}' was started", section);
+                            messages.add(am);
+                            continue;
+                        case INTERVAL_NOT_SET:
+                            logger.warn("Interval for '{}' is not specified or <=0", section);
+                            break;
+                        case NO_MESSAGES:
+                            logger.warn("Messages for '{}' is not specified or empty", section);
+                            break;
+                        case NO_SERVERS:
+                            logger.warn("Servers for '{}' is not specified or empty", section);
+                            break;
+                    }
+                    logger.warn("'{}' was not started", section);
+                }
+                logger.info("Done");
+            }
+        }).delay(3, TimeUnit.SECONDS).schedule();
+    }
+
+    private boolean loadConfig() {
+        File config = new File(dataDirectory.toFile(), "config.yml");
+        config.getParentFile().mkdir();
+        try {
+            if (!config.exists()) {
+                try (InputStream in = SimpleAutoMessages.class.getClassLoader().getResourceAsStream("config.yml")) {
+                    Files.copy(in, config.toPath());
+                }
+            }
+            this.config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(config);
+        } catch (Exception ex) {
+            logger.error("Can not load or save config", ex);
+            return false;
+        }
+        return true;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public ProxyServer getProxyServer() {
+        return server;
+    }
+
+    public Configuration getConfig() {
+        return config;
+    }
+
+    public Path getDataDirectory() {
+        return dataDirectory;
+    }
+}
